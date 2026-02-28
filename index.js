919504e3bc7897685de99f365801c8ee30a7fb9b
BereketTadesse <berekettadesse1244@gmail.com>
2026-03-01 00:33:47 +0300

    fixing cors

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import connectDB  from "./src/config/DB.js";
+import cors from "cors";
 
+import userRoutes from "./src/routes/user.route.js";
 dotenv.config({path: "./src/.env"});
 
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
-
-
-
-
-import userRoutes from "./src/routes/user.route.js";
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
 app.use("/api/users", userRoutes);
 
 
